a1e58a34ec5e9b3cf39f71da2fccd5cf7fa57e77
Fauzan <fauzanabdulbasith@gmail.com>
2026-04-13 12:45:11 +0700

    fix: enforce useCdn to false for fresh data retrieval in Sanity client configuration

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,10 +1,10 @@
 import { createClient } from "next-sanity";
-import { apiVersion, dataset, projectId, useCdn } from "../env";
+import { apiVersion, dataset, projectId } from "../env";
 
 export const client = createClient({
   projectId,
   dataset,
   apiVersion,
-  useCdn,
+  useCdn: false, // 👈 Paksa false agar selalu narik data fresh
   perspective: "published",
 });
